035c5a4b51d60b623cdf105689acf27a47bfc415
CaenDeSilva <caen@desilva.se>
2022-02-17 22:47:12 +0100

    Add documentation and let authors manage their posts

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,21 +8,33 @@ use Illuminate\Support\Facades\Gate;
 
 class DashboardController extends Controller
 {
-   
 
     /**
      * Display the view.
      *
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function show()
+    public function show(Request $request)
     {
+        // Authorize the request
         if (!Gate::allows('access-dashboards')) {
             abort(403);
         }
         
+        // If the user is an admin they can manage all posts
+        if ($request->user()->is_admin) {
+            $posts = Post::all();
+        }
+
+        // Otherwise if the user is an author we show their posts
+        elseif ($request->user()->is_author) {
+            $posts = $request->user()->posts;
+        }
+
+        // Return the view with the data we prepared
         return view('dashboard', [
-            'posts' => Post::all() ?? false,
+            'posts' => $posts ?? false,
         ]);
     }
 }
