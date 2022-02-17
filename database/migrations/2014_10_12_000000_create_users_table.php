097c81a56436f9392267d42ec0147d1f26ad5960
CaenDeSilva <caen@desilva.se>
2022-02-17 19:08:25 +0100

    Add admin and author attributes to user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,9 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+
+            $table->boolean('is_admin')->default(false);
+            $table->boolean('is_author')->default(false);
             $table->timestamps();
         });
     }
