dbeee664d3998717653a8f6b59b291ca3b1179f0
CaenDeSilva <caen@desilva.se>
2022-02-17 15:43:23 +0100

    Remove legacy code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,13 @@
 <x-app-layout>
     <div class="relative flex items-top justify-center sm:items-center py-4 sm:pt-0">
         <section class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div v-if="posts">
             @if($posts)
-                <header class="text-center py-5 mt-5 mb-3">
-                    <h1 class="text-3xl font-bold dark:text-white my-3">{{ config('app.name') }}</h1>
-                    <h2 class="text-2xl font-medium dark:text-white">Latest Blog Posts</h2>
-                </header>
+            <header class="text-center py-5 mt-5 mb-3">
+                <h1 class="text-3xl font-bold dark:text-white my-3">{{ config('app.name') }}</h1>
+                <h2 class="text-2xl font-medium dark:text-white">Latest Blog Posts</h2>
+            </header>
 
-                <livewire:latest-blog-posts />
-            </div>
+            <livewire:latest-blog-posts />
             @else
             <h2>There are no posts here!</h2>
             @endif
