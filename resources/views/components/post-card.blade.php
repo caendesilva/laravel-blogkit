0db25d95ef851b92afdc93399cf8f0de9907a933
CaenDeSilva <caen@desilva.se>
2022-02-17 20:22:57 +0100

    Hide text overflow on long content, such as links

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -10,7 +10,7 @@
                 {{ $post->title }}
             </h3>
         </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 overflow-hidden text-ellipsis">
             {{ $post->description }}
         </p>
         <a href="{{ route('post.show', $post) }}" class="mt-auto w-fit inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
