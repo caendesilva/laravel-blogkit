baed24e3d494efd16535900c311607053d077918
CaenDeSilva <caen@desilva.se>
2022-02-18 12:01:39 +0100

    Cast boolean attributes to boolean types

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,6 +40,8 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'is_admin' => 'boolean',
+        'is_author' => 'boolean',
     ];
 
     /**
