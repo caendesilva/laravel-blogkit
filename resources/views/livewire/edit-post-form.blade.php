9dfb00b2f5dc6af681098fcb2695dd3576221c58
CaenDeSilva <caen@desilva.se>
2022-02-18 16:57:09 +0100

    Add markdown editor to update form

--- resources/views/livewire/edit-post-form.blade.php
+++ resources/views/livewire/edit-post-form.blade.php
@@ -21,10 +21,37 @@
         </div>
 
         <div class="mt-3">
-            <x-label for="post.body" :value="__('Body*')" />
-            <x-textarea wire:model="post.body" class="block mt-1 w-full" required placeholder="Main content of the post" rows="8"></x-textarea>
+            <x-label for="content" :value="__('Body*')" />
+            <x-textarea id="content" name="content" wire:model.defer="post.body" class="block mt-1 w-full" placeholder="Main content of the post" rows="8"></x-textarea>
             @error('post.body') <span class="text-red-500">{{ $message }}</span> @enderror
-        </div>
+        </div>        
+
+        @push('scripts')
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">
+        <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
+        <style>
+            .editor-toolbar {
+                background-color: #fff;
+                opacity: 0.9;
+            }
+        </style>
+        <script>
+            // Initiate the editor
+            var simplemde = new SimpleMDE({
+                autosave: {
+                    enabled: true,
+                    uniqueId: 'draft-' + '{{ $post->id }}',
+                },
+                forceSync: true,
+            });
+
+            // Fixes bug where livewire does not recognize the input of the texteditor
+            simplemde.codemirror.on("change", function(){
+                var contentarea = document.getElementById("content");
+                contentarea.dispatchEvent(new Event('input'));
+            });
+        </script>
+        @endpush
 
         <div class="flex items-center justify-end mt-4">
             <x-button class="ml-4">
