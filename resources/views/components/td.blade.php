c2ff3f2e2850b4574e8cebebc0cfc8a6beab0229
CaenDeSilva <caen@desilva.se>
2022-02-18 21:39:47 +0100

    Update component

--- resources/views/components/td.blade.php
+++ resources/views/components/td.blade.php
@@ -1,3 +1,3 @@
-<th {{ $attributes->merge(['class' => 'dark:bg-slate-800 border border-slate-600 p-3 lg:py-2']) }}>
+<td {{ $attributes->merge(['class' => 'dark:bg-slate-800 border border-slate-600 p-3 lg:py-2']) }}>
     {{ $slot }}
-</th>
+</td>
