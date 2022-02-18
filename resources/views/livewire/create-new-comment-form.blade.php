c064917fed4c07937a08402b4680a6dd636bed34
CaenDeSilva <caen@desilva.se>
2022-02-18 21:58:49 +0100

    Add reminder to comment form

--- resources/views/livewire/create-new-comment-form.blade.php
+++ resources/views/livewire/create-new-comment-form.blade.php
@@ -10,4 +10,6 @@
     </form>
 
     <div>@error('content') <span class="text-red-500">{{ $message }}</span> @enderror</div>
+
+    <div class="text-sm opacity-75 mt-2">Please be civil and respectful when leaving a comment!</div>
 </div>
