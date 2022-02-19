3b56f2bcea62dd876c0dedff6cbc51e7aba95bd3
CaenDeSilva <caen@desilva.se>
2022-02-19 20:12:51 +0100

    Remove linebreaks causing unindented spacing

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,3 +1,1 @@
-<a {{ $attributes->merge(['class' => 'text-indigo-500 hover:text-indigo-700 transition ease-in-out duration-150']) }}>
-    {{ $slot }}
-</a>
+<a {{ $attributes->merge(['class' => 'text-indigo-500 hover:text-indigo-700 transition ease-in-out duration-150']) }}>{{ $slot }}</a>
