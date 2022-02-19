45dc46d2efdbb817446666d1542a388863c6a7e0
CaenDeSilva <caen@desilva.se>
2022-02-19 20:59:09 +0100

    Increase width of post form

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="relative flex items-top justify-center sm:items-center py-4 sm:pt-0">
-        <div class="max-w-2xl w-full mx-auto sm:px-6 lg:px-8 my-16">
+        <div class="max-w-5xl w-full mx-auto sm:px-6 lg:px-8 my-16">
             <h1 class="text-3xl font-bold dark:text-white my-3">New Blog Post</h1>
             <section class="bg-white rounded-lg shadow-md dark:bg-gray-800 py-4 px-6 dark:text-white">
 				<form action="{{ route('posts.store') }}" method="POST" class="text-gray-900">
