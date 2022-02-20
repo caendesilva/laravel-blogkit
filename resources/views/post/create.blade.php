23604507caed27ba09818296faa34e1e83ce4b2c
CaenDeSilva <caen@desilva.se>
2022-02-20 16:50:58 +0100

    Sort fields into fieldsets

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -10,33 +10,43 @@
 				<form action="{{ route('posts.store') }}" method="POST" class="text-gray-900">
                     @csrf
 
-                    <div class="mt-3">
-                        <x-label for="title" :value="__('Title*')" />
-                        <x-input id="title" name="title" :value="old('title')" type="text" class="block mt-1 w-full" maxlength="255" required autofocus placeholder="The post title"/>
-                        @error('title') <span class="text-red-500">{{ $message }}</span> @enderror
-                    </div>
+                    <fieldset>
+                        <legend class="dark:text-white">Required Fields</legend>
+
+                        <div class="mt-3">
+                            <x-label for="title" :value="__('Title*')" />
+                            <x-input id="title" name="title" :value="old('title')" type="text" class="block mt-1 w-full" maxlength="255" required autofocus placeholder="The post title"/>
+                            @error('title') <span class="text-red-500">{{ $message }}</span> @enderror
+                        </div>
+
+                        <div class="mt-3">
+                            <x-label for="body" :value="__('Body*')" />
+                            <x-textarea id="body" name="body" class="block mt-1 w-full" placeholder="Main content of the post" rows="8">{{ old('body') }}</x-textarea>
+                            @error('body') <span class="text-red-500">{{ $message }}</span> @enderror
+                        </div>        
+                
+                        @if(config('blog.easyMDE.enabled'))
+                        <x-markdown-editor :draft_id="$draft_id" />
+                        @endif
+                    </fieldset>
             
-                    <div class="mt-3">
-                        <x-label for="description" :value="__('Description')" />
-                        <x-input id="description" name="description" :value="old('description')" type="text" class="block mt-1 w-full" maxlength="255" placeholder="The post excerpt"/>
-                        @error('description') <span class="text-red-500">{{ $message }}</span> @enderror
-                    </div>
+                    <fieldset>
+                        <legend class="dark:text-white">Optional Fields</legend>
+
+                        <div class="mt-3">
+                            <x-label for="description" :value="__('Description')" />
+                            <x-input id="description" name="description" :value="old('description')" type="text" class="block mt-1 w-full" maxlength="255" placeholder="The post excerpt"/>
+                            @error('description') <span class="text-red-500">{{ $message }}</span> @enderror
+                        </div>
+                
+                        <div class="mt-3">
+                            <x-label for="featured_image" :value="__('Featured image URI')" />
+                            <x-input id="featured_image" name="featured_image" :value="old('featured_image')" type="text" class="block mt-1 w-full" maxlength="255" placeholder="https://example.org/static/image.jpg"/>
+                            @error('featured_image') <span class="text-red-500">{{ $message }}</span> @enderror
+                        </div>
+                    </fieldset>
             
-                    <div class="mt-3">
-                        <x-label for="featured_image" :value="__('Featured image URI')" />
-                        <x-input id="featured_image" name="featured_image" :value="old('featured_image')" type="text" class="block mt-1 w-full" maxlength="255" placeholder="https://example.org/static/image.jpg"/>
-                        @error('featured_image') <span class="text-red-500">{{ $message }}</span> @enderror
-                    </div>
-            
-                    <div class="mt-3">
-                        <x-label for="body" :value="__('Body*')" />
-                        <x-textarea id="body" name="body" class="block mt-1 w-full" placeholder="Main content of the post" rows="8">{{ old('body') }}</x-textarea>
-                        @error('body') <span class="text-red-500">{{ $message }}</span> @enderror
-                    </div>        
-            
-                    @if(config('blog.easyMDE.enabled'))
-                    <x-markdown-editor :draft_id="$draft_id" />
-                    @endif
+                  
             
                     <div class="flex items-center justify-end mt-4">
                         <x-button type="submit" class="ml-4">
