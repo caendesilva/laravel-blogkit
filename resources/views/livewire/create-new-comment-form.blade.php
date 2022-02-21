e9f8e1e417ee7dcc47d3966a742f13a8a74ded99
Caen De Silva <caen@desilva.se>
2022-02-21 16:12:57 +0100

    Shortcut to submit comment with ctrl/cmd+enter

--- resources/views/livewire/create-new-comment-form.blade.php
+++ resources/views/livewire/create-new-comment-form.blade.php
@@ -1,6 +1,15 @@
 <div>
-    <form wire:submit.prevent="save" class="text-gray-900 sm:flex">
-        <x-textarea wire:model.defer="content" class="w-full" placeholder="Post a comment" rows="1" required maxlength="1024"></x-textarea>
+    <form id="create-new-comment-form" wire:submit.prevent="save" class="text-gray-900 sm:flex" x-data="{ ctrlPressed: false }">
+        <x-textarea 
+            wire:model.defer="content" class="w-full" placeholder="Post a comment" rows="1" required maxlength="1024"
+            @keydown.ctrl="ctrlPressed = true"
+            @keyup.ctrl="ctrlPressed = false"
+            @keydown.enter.prevent="
+                if (ctrlPressed) {
+                    $wire.save(); 
+                }
+            "
+            ></x-textarea>
 
         <div class="flex items-center justify-end mt-4 sm:my-auto">
             <x-button class="ml-4 h-10" wire:loading.attribute="disabled">
