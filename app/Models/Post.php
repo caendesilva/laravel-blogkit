8ff67f8c3994a749cf29cee34964b7fc0dc53bfa
Caen De Silva <caen@desilva.se>
2022-02-22 22:21:27 +0100

    Add the publish date to the forms

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -24,6 +24,7 @@ class Post extends Model
         'description',
         'featured_image',
         'tags',
+        'published_at',
     ];
 
     /**
--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -55,13 +55,35 @@
                             <x-input id="featured_image" name="featured_image" :value="old('featured_image') ?? $post->featured_image" type="text" class="block mt-1 w-full" maxlength="255" placeholder="https://example.org/static/image.jpg"/>
                             @error('featured_image') <span class="text-red-500">{{ $message }}</span> @enderror
                         </div>
+                        
+                        <div class="mt-3">
+                            <x-label for="published_at" :value="__('When should the post be published?')" />
+                            <div class="flex flex-row items-center">
+                                <div>
+                                    <x-input id="published_at" name="published_at" :value="optional($post->published_at)->format('Y-m-d\TH:i')" type="datetime-local" class="block mt-1 w-full" placeholder="{{ now() }}"/>
+                                    @error('published_at') <span class="text-red-500">{{ $message }}</span> @enderror
+                                </div>
+                            </div>
+                            
+                            <x-label for="published_at">
+                                <small>You can also <button class="text-indigo-500 dark:text-indigo-300" title="Clear the input field" type="button" role="button" onclick="clearPublishedAtInput()">leave it blank</button> to save the post as a draft.</small>
+                            </x-label>
+                        </div>
 
                         @if(config('blog.withTags'))
                         <livewire:tag-manager :currenttags="$post->tags" />
                         @endif
                     </fieldset>
 
-                    <div class="flex items-center justify-end mt-4">
+                    <div class="flex items-center justify-end mt-4 mb-2">
+                        <div>
+                            <div class="flex flex-row items-center">
+                                <x-label class="cursor-pointer" for="is_draft" :value="__('Save as draft')" title="Saves the post without a publish date, making it hidden."/>
+                                <x-input id="is_draft" name="is_draft" value="1" :checked="old('is_draft') || isset($post) && $post->published_at === null" type="checkbox" class="block mx-2 cursor-pointer" title="Press to toggle"/>
+                            </div>
+                            @error('is_draft') <span class="text-red-500">{{ $message }}</span> @enderror
+                        </div>
+
                         <x-button type="submit" class="ml-4">
                             {{ __('Save') }}
                         </x-button>
@@ -70,4 +92,12 @@
 			</section>
         </div>
     </div>
+
+    @push('scripts')
+    <script>
+        function clearPublishedAtInput() { 
+            document.getElementById('published_at').value = null;
+        }
+    </script>
+    @endpush
 </x-app-layout>
